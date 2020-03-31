75329ed1a8b8e1f137d93fb4daa8c9b780cc9b67
MarcoANT9 <acalde27@hotmail.com>
2020-03-31 01:15:24 +0000

    setup mysql test: First Commit

---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+-- Creates the "hbnb_test_db" database with the user "hbnb_test" in localhost.
+-- The password for the that user will be "hbnb_test_pwd" and have all
+-- privileges on this database. The user has only SELECT privileges on the
+-- database called "performance_schema".
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
