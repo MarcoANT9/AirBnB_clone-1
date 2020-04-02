a547c10264264ebf760111c9d1bea23b09a2da82
Mauricio-KND <1299@holbertonschool.com>
2020-04-02 23:45:42 +0000

    Update of State. Task 6.

--- models/state.py
+++ models/state.py
@@ -2,7 +2,10 @@
 """This is the state class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+import models
+from models.city import City
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class State(BaseModel, Base):
     """This is the class for State
@@ -11,3 +14,18 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """Return list of City instances with state_id equal to current
+            State.id
+            """
+            list_cities = []
+            for city in models.storage.all(City).values():
+                if self.id == city.state_id:
+                    list_cities.append(city)
+            return list_cities
