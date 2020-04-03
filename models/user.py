a4f2bf53bd05ec442f8f1a4f808fb87e6ad4c0f0
MarcoANT9 <acalde27@hotmail.com>
2020-04-03 02:24:48 +0000

    Task 7: user upload

--- models/user.py
+++ models/user.py
@@ -23,7 +23,4 @@ class User(BaseModel, Base):
     """----CLASS RELATIONSHIPS----"""
 
     places = relationship("Place", backref="user",
-                          cascade="all, delete-orphan")
-
-    reviews = relationship("Reviwe", backref="user",
-                           cascade="all, delete-orphan")
+                         cascade="all, delete-orphan")
