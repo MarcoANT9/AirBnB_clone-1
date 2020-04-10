7d6e7e532812f6a0c5d20323acdb33d909494599
MarcoANT9 <acalde27@hotmail.com>
2020-04-10 21:18:20 +0000

    Setup web static: Revised code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # This script sets up a web server for deployment of web_static.
 
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
 # ------------ Create folders if they don't exist ------------------- #
 
@@ -10,7 +11,7 @@ sudo mkdir -p data/web_static/shared/
 sudo mkdir -p data/web_static/releases/test/
 
 # ------------ Create fake html file -------------------------------- #
-echo "Hello Holberton" | sudo tee data/web_static/releases/test/index.html
+echo "Hello Holberton" > data/web_static/releases/test/index.html
 
 # ------------ Create simbolic link --------------------------------- #
 
@@ -22,24 +23,6 @@ sudo chown -R ubuntu:ubuntu data/
 
 # ------------ Configure nginx ---------------------------------------#
 
-printf %s "server {
-    listen 80;
-    listen [::]:80 default_server;
-    root   /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=sCNrK-n68CM;
-    }
-    error_page 404 /404.html;
-    location = /404.html {
-        internal;
-    }
-
-    location /hbnb_static {
-        alias /data/web_static/current/;
-        index index.html index.htm;
-    }
-
-}" > /etc/nginx/sites-available/default
+sed -i '\listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 
 sudo service nginx restart
