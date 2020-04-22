f5d770a2b86315a55e2a74a19ed49f55df7a395f
MarcoANT9 <acalde27@hotmail.com>
2020-04-22 22:00:24 +0000

    Flask Task 9: First Commit

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This script starts a Flask web application. """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(self):
+    """ Closes the database. """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_listing():
+    """ List all states. """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+@app.route('/cities_by_states', strict_slashes=False)
+def city_in_state_list():
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
