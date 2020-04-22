4b269b18529fd1f1aed627cbdde24b6130fc14f8
MarcoANT9 <acalde27@hotmail.com>
2020-04-22 22:01:19 +0000

    Flask Task 8: Module - Pep8 revision

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -20,6 +20,7 @@ def state_listing():
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
+
 @app.route('/cities_by_states', strict_slashes=False)
 def city_in_state_list():
     states = storage.all(State).values()
