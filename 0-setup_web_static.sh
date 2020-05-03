83187f39a2e68429cbb46633dfb1616a5ed400d2
MarcoANT9 <acalde27@hotmail.com>
2020-05-03 23:05:33 +0000

    Server setup: Correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,28 +4,14 @@
 apt-get -y update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
-
-# ------------ Create folders if they don't exist ------------------- #
-
 mkdir -p /data/
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-
-# ------------ Create fake html file -------------------------------- #
 echo "Hello Holberton" > data/web_static/releases/test/index.html
-
-# ------------ Create simbolic link --------------------------------- #
-
 ln -sf data/web_static/releases/test/ data/web_static/current
-
-# ------------ Change ownership of data folder ---------------------- #
-
 chown -R ubuntu:ubuntu data/
-
-# ------------ Configure nginx ---------------------------------------#
-
 printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server ipv6only=on;
@@ -39,5 +25,4 @@ printf %s "server {
     alias /data/web_static/current/;
     }
 }" > /etc/nginx/sites-available/default
-
 service nginx restart
