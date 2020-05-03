d34b3a477ee1f572513e48c80e138ab48b7254c1
MarcoANT9 <acalde27@hotmail.com>
2020-05-03 23:18:08 +0000

    Server setup: Correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
-# This script sets up a web server for deployment of web_static.
+# config the server
 
-apt-get -y update
+apt-get update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
-mkdir -p data/
-mkdir -p data/web_static/
-mkdir -p data/web_static/releases/
-mkdir -p data/web_static/shared/
-mkdir -p data/web_static/releases/test/
-echo "Hello Holberton" > data/web_static/releases/test/index.html
-ln -sf data/web_static/releases/test/ data/web_static/current
-chown -R ubuntu:ubuntu data/
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu: /data/
 printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server ipv6only=on;
-    root /usr/share/nginx/html;
+listen 80 default_server;
+listen [::]:80 default_server ipv6only=on;
+root /usr/share/nginx/html;
         index index.html index.htm;
-    server_name localhost;
-    location / {
+server_name localhost;
+location / {
                 try_files $uri $uri/ =404;
-    }
-    location /hbnb_static/ {
-    alias /data/web_static/current/;
-    }
+}
+location /hbnb_static/ {
+alias /data/web_static/current/;
+}
 }" > /etc/nginx/sites-available/default
 service nginx restart
