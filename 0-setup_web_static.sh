beda258073f7665a06adbb6729c2ff1af363a2fb
MarcoANT9 <acalde27@hotmail.com>
2020-05-03 23:01:47 +0000

    Server setup: Correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,8 +7,11 @@ sudo ufw allow 'Nginx HTTP'
 
 # ------------ Create folders if they don't exist ------------------- #
 
-sudo mkdir -p data/web_static/shared/
-sudo mkdir -p data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
 # ------------ Create fake html file -------------------------------- #
 echo "Hello Holberton" > data/web_static/releases/test/index.html
@@ -23,6 +26,18 @@ sudo chown -R ubuntu:ubuntu data/
 
 # ------------ Configure nginx ---------------------------------------#
 
-sed -i '\listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server ipv6only=on;
+    root /usr/share/nginx/html;
+        index index.html index.htm;
+    server_name localhost;
+    location / {
+                try_files $uri $uri/ =404;
+    }
+    location /hbnb_static/ {
+    alias /data/web_static/current/;
+    }
+}" > /etc/nginx/sites-available/default
 
 sudo service nginx restart
